fd78fb59cd7c49bf76a7fcc748f0beeb96205f69
Matthew Marion Moncada <misakamikotocsr@gmail.com>
2021-09-27 20:12:57 -0600

    redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice'
+import { useSelector } from 'react-redux'
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars)
+    // console.log(cars);
+
     return (
         <Container>
             <a>
                 <img src='/images/logo.svg' alt='logo' />
             </a>
           <Menu>
-            <a href='#'>Model S</a>
-            <a href='#'>Model 3</a>
-            <a href='#'>Model X</a>
-            <a href='#'>Model Y</a>
+              {cars && cars.map((car, index)=>(
+                <a key={index} href='#'>{car}</a>
+              ))}
           </Menu>
           <RightMenu>
               <a href='#'>Shop</a>
               <a href='#'>Tesla Account</a>
-              <MenuIcon></MenuIcon>
+              <MenuIconContainer>
+                <MenuIcon onClick={() =>setBurgerStatus(true)}></MenuIcon>
+              </MenuIconContainer>
+              
           </RightMenu>
+          <BurgerNav show={burgerStatus}>
+              <CloseWrapper>
+                <CustomClose onClick={() =>setBurgerStatus(false)} />
+              </CloseWrapper>
+              {cars && cars.map((car, index)=>(
+                <li key={index}><a href='#'>{car}</a></li>
+              ))}
+              <li><a href='#'>Existing Inventory</a></li>
+              <li><a href='#'>Used Inventory</a></li>
+              <li><a href='#'>Trade-in</a></li>
+              <li><a href='#'>Cybertruck</a></li>
+              <li><a href='#'>Roadster</a></li>
+          </BurgerNav>
         </Container>
     )
 }
@@ -35,6 +57,7 @@ const Container = styled.div`
       top: 0; // stick to top
       left: 0; // stick to left
       right: 0; // stick to right
+      z-index: 99;
 `
 
 const Menu = styled.div`
@@ -63,6 +86,43 @@ const RightMenu = styled.div`
         text-transform: uppercase;
         margin-right: 10px;
     }
+`
+
+const BurgerNav = styled.div`
+     position: fixed;
+     top: 0;
+     bottom: 0;
+     right: 0;
+     background-color: #fff;
+     width: 300px;
+     z-index: 9;
+     list-style: none;
+     padding: 20px;
+     display: flex;
+     flex-direction: column;
+     text-align: start;
+     transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+     transition: transform 0.2s;
+     li {
+         padding: 15px 0;
+         border-bottom: 1px solid rgba(0,0,0, 0.2);
+         a {
+             font-weight: 600;
+         }
+     }
+`
+
+const CustomClose = styled(CloseIcon)`
+     cursor: pointer;
+`
+
+const MenuIconContainer = styled.div`
+     cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
 `
 
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade'
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{ title }</h1>
-                <p>{ description }</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{ title }</h1>
+                    <p>{ description }</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                    { leftBtnText }
-                    </LeftButton>
-                    {/* if right btn exists, show, else dont */}
-                    { rightBtnText &&
-                      <RightButton>
-                       { rightBtnText }
-                      </RightButton>
-                    }
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                        { leftBtnText }
+                        </LeftButton>
+                        {/* if right btn exists, show, else dont */}
+                        { rightBtnText &&
+                        <RightButton>
+                        { rightBtnText }
+                        </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src='/images/down-arrow.svg'></DownArrow>
             </Buttons>
         </Wrap>
@@ -29,6 +34,7 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImg 
 export default Section
 
 const Wrap = styled.div`
+     z-index: 10;
      width: 100vw;
      height: 100vh;
      background-image: url('/images/model-s.jpg');
